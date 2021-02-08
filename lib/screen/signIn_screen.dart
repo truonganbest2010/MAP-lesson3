ba79db3750cde9945514d2a737335561929f860a
AnTruong <truonganbest2010@gmail.com>
2021-02-07 19:06:50 -0600

    ini

---
+++ lib/screen/signIn_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  static const routeName = '/signInScreen';
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInState();
+  }
+}
+
+class _SignInState extends State<SignInScreen> {
+  _Controller con;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
+  void render(fn) => setState(fn);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                validator: con.validateEmail,
+                onSaved: con.saveEmail,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+                autocorrect: false,
+                validator: con.validatePassword,
+                onSaved: con.savePassword,
+              ),
+              RaisedButton(
+                onPressed: con.signIn,
+                child: Text('Sign In', style: Theme.of(context).textTheme.button),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Controller {
+  _SignInState state;
+  _Controller(this.state);
+  String email, password;
+
+  String validateEmail(String value) {
+    if (value.contains('@') && value.contains('.'))
+      return null;
+    else
+      return 'invalid email address';
+  }
+
+  void saveEmail(String value) {
+    email = value;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'too short';
+    else
+      return null;
+  }
+
+  void savePassword(String value) {
+    password = value;
+  }
+
+  void signIn() {
+    if (!state.formKey.currentState.validate()) return;
+    state.formKey.currentState.save();
+  }
+}
